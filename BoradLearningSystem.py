ea3b56ef2245d556e850397761caf537a945c152
stan-maze <84319461+stan-maze@users.noreply.github.com>
2023-11-18 17:09:32 +0800

    基础BLS代码学习注释,跑通mnist

--- BoradLearningSystem.py
+++ BoradLearningSystem.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 
 from scipy import linalg as LA
-from scipy import io as scio
 from numpy import random
 from sklearn import preprocessing
 
@@ -20,6 +19,7 @@ class BLS:
     def GridSearchCV_BLS(NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2**-30):
         return 0
 
+    # tansig也是常用的激活函数, 双曲正切, sigmoid也称logsig
     def tansig(self, x):
         return (2/(1+np.exp(-2*x)))-1
     
@@ -27,14 +27,19 @@ class BLS:
         return np.maximum(0, x)
 
     def pinv(self, A, reg):
+        # 公式2, reg是λ, 注意是At*A, 论文确实有误
         return np.mat(reg*np.eye(A.shape[1])+A.T.dot(A)).I.dot(A.T)
     
     def cls_pinv(self, matrix):
+        # 分类情况, 只改用极小的C? 对应公式3
         return np.mat(self.C * np.eye(matrix.shape[1]) + matrix.T.dot(matrix)).I.dot(matrix.T)
 
     '''
     参数压缩
     '''
+    # 还不能理解, 软阈值操作
+    # 输出的z是经过软阈值处理后的结果。如果输入矩阵a的某个元素大于b，那么相应的z元素将等于a的该元素减去b；如果a的某个元素小于-b，那么相应的z元素将等于a的该元素加上b；如果在-b和b之间，相应的z元素将等于0。
+    # 这种软阈值操作在信号处理、统计学和机器学习中常用于稀疏性推动，使得一些元素趋向于零，从而达到降维或者特征选择的效果。
     def shrinkage(self, a, b):
         z = np.maximum(a - b, 0) - np.maximum(-a - b, 0)
         return z
@@ -42,7 +47,10 @@ class BLS:
     '''
     参数稀疏化
     '''
+    # 基于ADMM的Sparse Autoencoder
+    # A对应Z,b对应X, 在给定的变换结果Z和原数据X上, 找到一个更稀疏的变换矩阵W
     def sparse_bls(self, A, b):
+        # A一组特征[数据量,FeatureNodes], b原数据[数据量,特征维数(+1)]
         lam = 0.001
         itrs = 50
         AA = np.dot(A.T, A)
@@ -69,35 +77,48 @@ class BLSRegressor(BLS):
     def fit(self, train_x, train_y):
         u = 0
         WF = list()
+        # 总共有FeatureNodes个特征节点,每个节点有FeatureWindows大小, 不知道为什么不按照FeatureNodes来迭代, 可能是特征选择的通常做法?
         for i in range(self.FeatureWindows):
             random.seed(i+u)
             WeightFea = 2*random.randn(train_x.shape[1]+1, self.FeatureNodes)-1
             WF.append(WeightFea)
+        # 增强节点的第一种形式 nk->h, 有EnhancementNodes个， 为什么nk+1?
         self.WeightEnhan = 2*random.randn(self.FeatureWindows*self.FeatureNodes+1, self.EnhancementNodes)-1
 
+        # 出于还没搞明白的原因，数据拼接了一个0.1，上面的权重矩阵也对应是train_x.shape[1]+1, 人为添加bias吗
         H1 = np.hstack([train_x, 0.1 * np.ones([train_x.shape[0], 1])])
+        
         y = np.zeros([train_x.shape[0], self.FeatureWindows*self.FeatureNodes])
         self.WFSparse = list()
         self.distOfMaxAndMin = np.zeros(self.FeatureWindows)
         self.meanOfEachWindow = np.zeros(self.FeatureWindows)
         for i in range(self.FeatureWindows):
             WeightFea = WF[i]
+            # A1那是[i, n]中的一组值, 换句话说就是 特征节点的第i个值的组合
             A1 = H1.dot(WeightFea)
+            # 出于还没搞明白的原因，这里scaler了一下
             scaler1 = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(A1)
             A1 = scaler1.transform(A1)
+            # A一组特征[数据量,FeatureNodes], b原数据[数据量,特征维数(+1)]
             WeightFeaSparse = self.sparse_bls(A1, H1).T
             self.WFSparse.append(WeightFeaSparse)
-
+            # 保持变换结果A1的更稀疏的权重, T1是在该权重下A1的近似
             T1 = H1.dot(WeightFeaSparse)
+            # 将T1中心化
             self.meanOfEachWindow[i] = T1.mean()
             self.distOfMaxAndMin[i] = T1.max() - T1.min()
             T1 = (T1 - self.meanOfEachWindow[i]) / self.distOfMaxAndMin[i]
+            # 将变换结果T1记入y, 按照一组一组, 第i个FeatureWindows里面是所有FeatureNodes对应值的排列
             y[:, self.FeatureNodes * i:self.FeatureNodes * (i+1)] = T1
-
+        
+        # 特征节点输入之前也要拼接0.1, 对应项目权重矩阵的nk+1
         H2 = np.hstack([y, 0.1 * np.ones([y.shape[0], 1])])
+        # 同样直接使用初始化的WeightEnhan得到增强节点, 这里没有再进一步稀疏化
         T2 = H2.dot(self.WeightEnhan)
         T2 = self.tansig(T2)
+        # 最终的输入,将Z(y)和H拼接
         T3 = np.hstack([y, T2])
+        # 文章公式2, 就是计算伪逆然后得到W, 不还是用self.c??
         self.WeightTop = self.pinv(T3, self.C).dot(train_y)
 
         print(self.WeightTop.shape)
@@ -106,12 +127,13 @@ class BLSRegressor(BLS):
         return NetoutTrain
 
     def predict(self, test_x):
-        # 预测输出
+        # 使用训练得到的权重, 按顺序把所有节点值计算好
         HH1 = np.hstack([test_x, 0.1 * np.ones([test_x.shape[0], 1])])
         yy1 = np.zeros([test_x.shape[0], self.FeatureWindows * self.FeatureNodes])
         for i in range(self.FeatureWindows):
             WeightFeaSparse = self.WFSparse[i]
             TT1 = HH1.dot(WeightFeaSparse)
+            # 注意仍然要中心化
             TT1 = (TT1 - self.meanOfEachWindow[i]) / self.distOfMaxAndMin[i]
             yy1[:, self.FeatureNodes * i:self.FeatureNodes*(i+1)] = TT1
         HH2 = np.hstack([yy1, 0.1 * np.ones([yy1.shape[0], 1])])
@@ -162,17 +184,20 @@ class BLSClassifier(BLS):
 
             self.Dist_MaxAndMin.append(np.max(Output_EachWindow, axis=0) - np.min(Output_EachWindow, axis=0))  # 计算损失函数
             self.Min_EachWindow.append(np.min(Output_EachWindow, axis=0))
+            # 这里用的是最小值来中心化? 为什么称之为计算损失函数?
             Output_EachWindow = (Output_EachWindow - self.Min_EachWindow[i]) / self.Dist_MaxAndMin[i]
             # 计算特征层最终输出
             Output_FeatureMappingLayer[:, self.FeatureNodes * i:self.FeatureNodes * (i + 1)] = Output_EachWindow
 
-        # 增强层
+        # 这里使用了什么技巧?为什么特征节点和增强节点分开计算了?
         train_ori_hance = np.hstack([train_x])
         train_x_enhance = preprocessing.scale(train_ori_hance, axis=1)
         Input_EnhanceLayerWithBias = np.hstack([train_x_enhance, 0.1 * np.ones((train_x_enhance.shape[0], 1))])
-
+        
+        # 为什么使用LA.orth嵌套了一下增强节点的权重矩阵?
         self.W_EnhanceLayer = LA.orth(2 * random.randn(train_x_enhance.shape[1] + 1, self.EnhancementNodes)) - 1
 
+        # 为什么用self.S?
         Temp_Output_EnhanceLayer = np.dot(Input_EnhanceLayerWithBias, self.W_EnhanceLayer)  # 计算增强层中间态
         self.Parameter_Shrink = self.S / np.max(Temp_Output_EnhanceLayer)
         Output_EnhanceLayer = self.relu(Temp_Output_EnhanceLayer * self.Parameter_Shrink)  # 计算增强层最终输出
@@ -203,8 +228,10 @@ class BLSClassifier(BLS):
 
         for i in range(self.FeatureWindows):
             Output_EachWindowTest = np.dot(Feature_InputDataWithBiasTest, self.Beta1_EachWindow[i])
+            # 这里为什么不对称?self.ymax, self.ymin并没有改动过, 有什么伏笔吗, 还有什么更完善的实现方式?
             Output_FeatureMappingLayerTest[:, self.FeatureNodes * i:self.FeatureNodes * (i + 1)] = (self.ymax - self.ymin) * (Output_EachWindowTest - self.Min_EachWindow[i]) / self.Dist_MaxAndMin[i] - self.ymin
 
+        # 这里的增强节点不是由特征节点计算来的, 那跟单层前馈有什么区别
         test_ori_hance = np.hstack([test_x])
         test_x_enhance = test_ori_hance
         Input_EnhanceLayerWithBiasTest = np.hstack([test_x_enhance, 0.1 * np.ones((test_x_enhance.shape[0], 1))])
--- test_demo.py
+++ test_demo.py
@@ -1,17 +1,19 @@
 from BoradLearningSystem import BLSRegressor, BLSClassifier
 from BroadLearningSystemBasedAutoEncoder import BLSAEExtractor, StackedBLSAEExtractor
 from sklearn.datasets import load_iris, load_breast_cancer
+import numpy as np
+from scipy import io as scio
 
 # -- 模型初始化 --
-# regressor = BLSRegressor(NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2**-30)
-# classifier = BLSClassifier()
-extractor = BLSAEExtractor()
-stack_extractor = StackedBLSAEExtractor()
-stack_extractor_2 = StackedBLSAEExtractor(is_multi_feature=True)
+regressor = BLSRegressor(NumFeatureNodes=10, NumWindows=10, NumEnhance=10, S=0.5, C=2**-30)
+classifier = BLSClassifier()
+# extractor = BLSAEExtractor()
+# stack_extractor = StackedBLSAEExtractor()
+# stack_extractor_2 = StackedBLSAEExtractor(is_multi_feature=True)
 
 
-# -- mat文件 加载minst --
-# dataFile = 'code/mnist.mat'
+# # -- mat文件 加载minst --
+# dataFile = 'data/mnist.mat'
 # data = scio.loadmat(dataFile)
 # traindata = np.double(data['train_x']/255)
 # trainlabel = np.double(data['train_y'])
@@ -20,35 +22,61 @@ stack_extractor_2 = StackedBLSAEExtractor(is_multi_feature=True)
 
 
 # -- sklearn 加载回归数据 --
-# dataset = load_iris()
-# data, label = dataset['data'], dataset['target']
+dataset = load_iris()
+data, label = dataset['data'], dataset['target']
 # print(label)
 
 # -- sklearn 加载分类数据 --
-cls_dataset = load_breast_cancer()
-cls_data, cls_label = cls_dataset['data'], cls_dataset['target']
-print(cls_data.shape)
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+# cls_dataset = load_breast_cancer()
+# cls_data, cls_label = cls_dataset['data'], cls_dataset['target']
+# print(cls_data.shape)
+# 重写代码
+# mnist = fetch_openml("mnist_784")
+# X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=0.2, random_state=42)
+# print(mnist.data.shape)
 
-# -- BLSAE --
-feature_encode_x = extractor.fit(cls_data)
-print(feature_encode_x.shape)
+# # -- BLSAE --
+# feature_encode_x = extractor.fit(cls_data)
+# print(feature_encode_x.shape)
 
-# -- Stacked BLSAE --
-feature_encode_x = stack_extractor.fit(cls_data)
-print(feature_encode_x.shape)
+# # -- Stacked BLSAE --
+# feature_encode_x = stack_extractor.fit(cls_data)
+# print(feature_encode_x.shape)
 
-feature_encode_x = stack_extractor_2.fit(cls_data)
-print(feature_encode_x)
+# feature_encode_x = stack_extractor_2.fit(cls_data)
+# print(feature_encode_x)
 
-# -- 回归 --
+# # -- 回归 --
 # train_output = regressor.fit(data, label)
 # print(train_output.shape)
 # predict_output = regressor.predict(data)
 # print(predict_output.shape)
 
 # -- 分类 --
-# train_output = classifier.fit(cls_data, cls_label, is_excel_label=True)
-# print(train_output)
-# predict_output = classifier.predict(cls_data)
-# print(predict_output)
+from sklearn.datasets import load_digits
+dataFile = 'data/mnist.mat'
+mnist_data = scio.loadmat(dataFile)
+# mnist_data = fetch_openml("mnist_784", version=1, cache=True)
+keys = mnist_data.keys()
+print(keys)
+X,y = mnist_data['Z'], mnist_data['y']
+y = np.array([l[0] for l in y ])
+# X = X/255
+print(X[:2], y[:2])
+print(X.shape, y.shape)
+# mnist_data = load_digits()
+# print(mnist_data['data'][:2], mnist_data['target'][:2])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.metrics import accuracy_score
+
+train_output = classifier.fit(X_train, y_train, is_excel_label=True)
+print(train_output[0])
+predict_output = classifier.predict(X_test)
+# print(len(predict_output), predict_output[0], y_test[0])
+acc = accuracy_score(y_test, predict_output)
+print(acc)
 
